6d03ffac9033fe5d24b6e39765b5df16ce4bf419
AnkanHalder <35003965+AnkanHalder@users.noreply.github.com>
2018-06-19 21:42:45 +0530

    Multi Color with Effects

--- canvas.js
+++ canvas.js
@@ -6,6 +6,37 @@ canvas.height=window.innerHeight;
 var c=canvas.getContext('2d');
  c.fillStyle='orange';
  var circlearray=[];
+ var maxRadius=60;
+ var distance=150;
+ var density=500;
+
+ var mouse={
+   x:undefined,
+   y:undefined
+ }
+
+ window.addEventListener('mousemove',function(event){
+   mouse.x=event.x;
+   mouse.y=event.y;
+ });
+ window.addEventListener('resize',function(){
+   canvas.width=window.innerWidth;
+   canvas.height=window.innerHeight;
+   init();
+ });
+
+
+
+ var colors=[
+   '#9400D3',
+   '#4B0082',
+   '#0000FF',
+   '#00FF00',
+   '#FFFF00',
+   '#FF0000',
+   '#FF7F00'
+ ];
+
 
 
 function Circle(x,y,dx,dy,r){
@@ -14,12 +45,13 @@ function Circle(x,y,dx,dy,r){
   this.dx=dx;
   this.dy=dy;
   this.r=r;
+  this.minRadius=r;
+  this.color=colors[Math.floor(Math.random()*colors.length)];
 
   this.draw=function(){
     c.beginPath();
     c.arc(this.x,this.y,this.r,0,Math.PI*2,false);
-    c.strokeStyle="purple";
-    c.stroke();
+    c.fillStyle=this.color;
     c.fill();
   }
   this.conditions=function(){
@@ -31,18 +63,36 @@ function Circle(x,y,dx,dy,r){
     }
     this.x+=this.dx;
     this.y+=this.dy;
+
+    //Updations of Events
+    if(mouse.x-this.x<distance && mouse.x-this.x >-distance &&
+       mouse.y-this.y<distance && mouse.y-this.y >-distance){
+      if(this.r<maxRadius){
+        this.r+=4;
+      }
+    }
+    else if(this.r>this.minRadius){
+      this.r-=1;
+    }
   }
 }
 
-for(var i=0;i<100;i++){
-  var r=20;
-  var x=Math.random()*(innerWidth-2*r)+r;
-  var y=Math.random()*(innerHeight-2*r)+r;
-  var dy=(Math.random()-.5)*10;
-  var dx=(Math.random()-.5)*10;
+function init(){
 
-circlearray.push(new Circle(x,y,dx,dy,r));
+circlearray=[];
+  for(var i=0;i<density;i++){
+    var r=Math.random()*20 + 5;
+    var x=Math.random()*(innerWidth-2*r)+r;
+    var y=Math.random()*(innerHeight-2*r)+r;
+    var dy=(Math.random()-.5)*5;
+    var dx=(Math.random()-.5)*5;
+
+  circlearray.push(new Circle(x,y,dx,dy,r));
+  }
+
+
 }
+
 
 function animate(){
   requestAnimationFrame(animate);
@@ -53,4 +103,5 @@ for(var i=0;i<circlearray.length;i++){
 }
 
 }
+init();
 animate();
